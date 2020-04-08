84cdbe3befa615e7ab9a0107c822c9c8fc20901a
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-04-08 02:00:25 +0200

    fix memory leak mostly

--- func.hpp
+++ func.hpp
@@ -2,6 +2,7 @@ namespace callpy {
 
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
+#include "object.h"
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 #include <math.h>
@@ -22,6 +23,8 @@ void foo(int n, const double *x, double *c_lp, double *c_glp) {
 		PyObject *lp = PyObject_GetAttrString(main, "lp");
 		PyArrayObject *lp_val = (PyArrayObject*) PyObject_CallFunctionObjArgs(lp, np_x, NULL);
 		PyArray_ScalarAsCtype((PyObject*) lp_val, (void*) &c_lp);
+		Py_DECREF(lp);
+		Py_DECREF(lp_val);
 	}
 
 	if (c_glp != NULL)
@@ -31,6 +34,8 @@ void foo(int n, const double *x, double *c_lp, double *c_glp) {
 		double *c_glp_val = (double*) PyArray_DATA(glp_val);
 		for (int i=0; i<n; i++)
 			c_glp[i] = c_glp_val[i];
+		Py_DECREF(glp);
+		Py_DECREF(glp_val);
 	}
 
 	PyGILState_Release(gstate);
@@ -57,6 +62,7 @@ template <> var userfunc2(const Eigen::Matrix<var, -1, 1>& theta, std::ostream* 
     callpy::foo(theta.rows(), theta_val.data(), &fa, ga);
     std::vector<double> std_ga(theta.rows());
     std_ga.assign(ga, ga + theta.rows());
+    delete ga;
     vector<var> theta_std(theta.data(), theta.data() + theta.rows());
     return precomputed_gradients(fa, theta_std, std_ga);
 }
